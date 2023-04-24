18f302eff39b9addaac824bedd5f897f9cd9071e
Diganta123 <diganta15-4898@diu.edu.bd>
2023-04-25 00:25:03 +0600

    seive eratosthenes algorithm

--- Seive_of_Eratosthenes/seive.cpp
+++ Seive_of_Eratosthenes/seive.cpp
@@ -1,7 +1,37 @@
 #include<bits/stdc++.h>
 using namespace std;
+const int N = 1e7+10;
+vector <bool> isprime(N, 1);
+
 int main()
 {
-    
+
+    isprime[0]==isprime[1]==false;
+    for(int i = 2; i<N; i++)
+    {
+        if(isprime[i]==true)
+        {
+            for(int j = i*2; j<N; j+=i )
+            {
+                isprime[j]=false;
+            }
+        }
+    }
+
+
+
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int x; 
+        cin>>x;
+        if(isprime[x])
+        {
+            cout<<"prime"<<endl;
+        }
+        else 
+        cout<<"Not prime "<<endl;
+    }
     return 0;
 }
